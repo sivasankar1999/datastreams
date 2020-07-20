4651c69c57b04308f2332187a27a49288bc4cec1
sivasankar1999 <sivasankargannamani1999@gmail.com>
2020-07-20 22:08:49 +0530

    done

---
+++ DataStreams/src/main/java/epam_project/DataStreams/StreamAverage.java
@@ -0,0 +1,21 @@
+package epam_project.DataStreams;
+
+import java.util.*;
+
+public class StreamAverage {
+	public static double average(ArrayList<Integer> arr) {
+		return arr.stream().mapToInt(i->i).average().getAsDouble();
+	}
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		for(int i = 0;i<n;i++) {
+			arr.add(input.nextInt());
+		}
+		double result = average(arr);
+		System.out.println(result);
+	}
+
+}
---
+++ DataStreams/src/main/java/epam_project/DataStreams/StreamsA.java
@@ -0,0 +1,21 @@
+package epam_project.DataStreams;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StreamsA {
+	public static List<String> search(ArrayList<String> arr) {
+		return arr.parallelStream().filter(s->s.startsWith("a")).filter(s->s.length() == 3).collect(Collectors.toList());
+	}
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		ArrayList<String> arr = new ArrayList<String>();
+		for(int i = 0;i<n;i++) {
+			arr.add(input.next());
+		}
+		System.out.println(search(arr));
+	}
+
+}
